5a36fc910d85f3ed01018f83b8e94ce1589df3de
Nikkk <moskalevskiy1@gmail.com>
2024-11-30 18:41:20 +0300

    small changes

--- server/ImgRoute.js
+++ server/ImgRoute.js
@@ -6,13 +6,12 @@ const endPointImages = express.Router();
 
 // GET
 endPointImages.get('/static/:filename', (req, res) => {
-    const filename = req.params.filename; // Get the filename from the request
-    const filePath = path.join(__dirname, 'static', filename); // Full file path
-    const fallbackPath = path.join(__dirname, 'static', 'blank.jpg'); // Path to blank.jpg
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, 'static', filename);
+    const fallbackPath = path.join(__dirname, 'static', 'blank.jpg');
 
     fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
-            console.warn(`File not found: ${filename}. Serving blank.jpg instead.`);
             return res.sendFile(fallbackPath);
         }
 
